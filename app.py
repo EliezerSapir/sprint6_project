760d467381d33bcef44f566a6f53980dd9fe4a24
EliezerSapir <sapircreations@gmail.com>
2023-12-17 15:08:36 +0200

    with streamlit config

--- app.py
+++ app.py
@@ -6,23 +6,29 @@ df = pd.read_csv('vehicles_us.csv')
 
 st.header('Exploration of car sales over the years', divider='rainbow')
 
-st.subheader('Older cars and newer cars sell for the highest price. Car models from 1974-2013 sell on average for less than 15K dollars.')
+st.subheader('Model Year vs Price')
+st.write('Older cars and newer cars sell for the highest price. Car models from 1974-2013 sell on average for less than 15K dollars.')
 
 year = px.histogram(df, df.model_year, df.price, histfunc='avg')
 
-st.bar_chart(year)
+#st.bar_chart(year)
+st.plotly_chart(year)
 
-st.subheader('The graph below supports common sense, the new cars sell for the most while the fair and salvage categories consistently sell for the least.')
+st.subheader('Car Condition vs Price')
+st.write('The graph below supports common sense, the new cars sell for the most while the fair and salvage categories consistently sell for the least.')
 
 condition = px.histogram(df, df.condition, df.price, histfunc='avg')
 
-st.bar_chart(condition)
+#st.bar_chart(condition)
+st.plotly_chart(condition)
 
-st.subheader('The graph below takes this observation deeper, plotting price against model year, with colors marking the condition. We can see from this graph that the majority of cars in new condition were produced in the last 20 years. We can also easily see the outlier with the highest sale was from a car in good condition from 1999.')
+st.subheader('Model Year vs Price based on Car Condition')
+st.write('The graph below takes this observation deeper, plotting price against model year, with colors marking the condition. We can see from this graph that the majority of cars in new condition were produced in the last 20 years. We can also easily see the outlier with the highest sale was from a car in good condition from 1999.')
 
 fig = px.scatter(df, x="model_year", y="price", color='condition')
 
-st.scatter_chart(fig)
+#st.scatter_chart(fig)
+st.plotly_chart(fig)
 
 st.subheader('What can you expect to pay for a car from the year (in $):')
 choice = st.checkbox('1960')
@@ -43,6 +49,9 @@ if choice5:
 choice6 = st.checkbox('2010')
 if choice6:
     st.write(df[df['model_year'] == 2010]['price'].mean())
-choice7 = st.checkbox('2020')
+choice7 = st.checkbox('2019')
 if choice7:
-    st.write(df[df['model_year'] == 2020]['price'].mean())
+    st.write(df[df['model_year'] == 2019]['price'].mean())
+
+year = st.slider('Check a specific year', 1960, 2019, 1960)
+st.write("That will be ", df[df['model_year'] == year]['price'].mean(), 'dollars')
